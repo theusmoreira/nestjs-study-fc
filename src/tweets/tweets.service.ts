42454d154cd01239bd88ebc2dd592ef93ff4103c
Matheus Moreira <matheussm301@gmail.com>
2022-02-08 17:48:44 -0300

    feat: add mongo in nest, and tweet schema

--- src/tweets/tweets.service.ts
+++ src/tweets/tweets.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreateTweetDto } from './dto/create-tweet.dto';
 import { UpdateTweetDto } from './dto/update-tweet.dto';
+import { Tweet, TweetDocument } from './schemas/tweet.schema';
 
 @Injectable()
 export class TweetsService {
+  constructor(
+    @InjectModel(Tweet.name)
+    private readonly tweetModel: Model<TweetDocument>,
+  ) {}
+
   create(createTweetDto: CreateTweetDto) {
-    return 'This action adds a new tweet';
+    return this.tweetModel.create(createTweetDto);
   }
 
   findAll() {
-    return `This action returns all tweets`;
+    return this.tweetModel.find().exec();
   }
 
   findOne(id: number) {
